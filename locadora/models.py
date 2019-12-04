71777ded467259f964af8642c95d527938f41828
jorge <luizfonseca830@gmail.com>
2019-12-04 16:21:34 -0500

    lista de automoveis locados

--- locadora/models.py
+++ locadora/models.py
@@ -4,8 +4,8 @@ from automoveis.models import Automovel
 
 
 class Locacao(models.Model):
-    cliente = models.ForeignKey(Cliente, null=False, blank=False, on_delete=models.CASCADE)
-    automovel = models.ForeignKey(Automovel, null=False, blank=False, on_delete=models.CASCADE)
+    cliente = models.OneToOneField(Cliente, null=False, blank=False, on_delete=models.CASCADE)
+    automovel = models.OneToOneField(Automovel, null=False, blank=False, on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     data_inicio = models.DateField()
     data_final = models.DateField()
